2ade08c9e52f83d6733813a4faeb910e2cb07084
I hate iu <tranhung10122003@gmail.com>
2025-06-04 22:45:51 +0700

    feta

--- BE/medical/management/commands/load_medical_data.py
+++ BE/medical/management/commands/load_medical_data.py
@@ -3,7 +3,6 @@ from django.core.management.base import BaseCommand
 from medical.models import MedicalCondition, Symptom, ConditionSymptom, SpecialistRecommendation
 
 
-
 class Command(BaseCommand):
     help = 'Load sample medical data for 4 conditions'
 
